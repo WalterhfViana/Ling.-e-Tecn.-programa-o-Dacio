f5d6748ab9a73cca14528423c7471304a79bfd94
Walter Viana <walterhfviana@gmail.com>
2026-08-25 09:26:09 -0300

    Update multiplode2emaiormenor.c

--- multiplode2emaiormenor.c
+++ multiplode2emaiormenor.c
@@ -10,14 +10,9 @@ int main(int argc, char *argv[]) {
 	scanf("%d %d", &a ,&b );
 	
 	if(a>0 && b>0){
-		if(ar = (a%2))/* a multiplo 2 */{ 
-			printf("%d nao eh multiplo de 2\n", a);
-		}else{
-			printf("%d eh multiplo de 2\n", a);}
-		if(br = (b%2))/* b multiplo 2 */{
-			printf("%d nao eh multiplo de 2\n", b);
-		}else{
-			printf("%d eh multiplo de 2\n", b);}
+		if (a%2 == 0 && b%2 == 0){
+			printf("sao multiplos de 2\n");
+		}else{ printf("nao sao multiplos de 2\n");}
 		if(a>b) /* caso a seja maior que b*/{
 			if (a%b){
 			printf("nao sao multiplos entre si\n");
@@ -36,8 +31,7 @@ int main(int argc, char *argv[]) {
 			if(a>b){
 				printf("%d eh maior que %d\n", a,b);
 			}else{
-				printf("%d eh maior que %d\n",b ,a);}
-		};
+				printf("%d eh maior que %d\n",b ,a);}};
 	
 	return 0;
 }

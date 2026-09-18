3d56d98063a31e72231355eb8e7c1c8c724edd67
Walter Viana <walterhfviana@gmail.com>
2026-09-18 15:23:43 -0300

    Create Exerc1.c

---
+++ Listas/Lista1/Exerc1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+
+    int a, b;
+ 
+    printf("Digite o primeiro numero inteiro: \n");
+    scanf("%d", &a);
+ 
+    printf("Digite o segundo numero inteiro: \n");
+    scanf("%d", &b);
+ 
+    printf("Ordem inversa: %d %d\n", b, a);
+  
+    return 0;
+}

8a256fab42d4b3a733258cf3046a2ac06d094117
Walter Viana <walterhfviana@gmail.com>
2026-09-18 15:32:53 -0300

    Create Exerc6.c

---
+++ Listas/Lista02set/Exerc6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+	int n, antecessor, sucessor;
+	printf("Entre com o valor de N");
+	scanf("%d", &n);
+	printf(" o numero %d, seu antecessor %d e seu sucessor %d", n, n-1, n+1);
+	
+	
+	return 0;
+}
